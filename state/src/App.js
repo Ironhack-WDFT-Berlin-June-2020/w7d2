1e409114bf18983e0179ebe03eef493433e7e6ca
Jan Redmann <janredmann1@gmail.com>
2020-07-14 13:11:43 +0200

    add state

--- state/src/App.js
+++ state/src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    clickCount: 0,
+    liked: false
+  }
+
+  clickHandler = () => {
+    // console.log('clicked');
+    // this.setState({
+    //   clickCount: this.state.clickCount + 1
+    // })
+    console.log(this.state.clickCount);
+    this.setState((state, props) => ({
+      clickCount: state.clickCount + 1
+    }))
+  }
+
+  likeHandler = () => {
+    this.setState((state, props) => ({
+      liked: !state.liked
+    }))
+  }
+
+  render() {
+    // let heading = '';
+    // if (this.state.liked) {
+    //   heading = 'I like this component'
+    // } else {
+    //   heading = 'I do not like'
+    // }
+    return (
+      <>
+        <h1>Counter: {this.state.clickCount}</h1>
+        <button onClick={this.clickHandler}>Click me</button>
+        <h1>{this.state.liked ? 'I like this component' : 'I don\'t like this component'}</h1>
+        <button onClick={this.likeHandler}>{this.state.liked ? 'Unlike' : 'Like'}</button>
+      </>
+    )
+  }
 }
 
 export default App;
