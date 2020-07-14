17a56f8963e1d13b6858b727238cf081fe38357f
Jan Redmann <janredmann1@gmail.com>
2020-07-14 11:10:19 +0200

    add create-react-app

---
+++ create-react-app/src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import logo from '../logo.svg';
+// import { ReactComponent as Logo } from '../logo.svg';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    {/* <Logo className="App-logo" /> */}
+                    <h1>Welcome Ironhacker</h1>
+                    <h3>You are ready to take this to the next level!</h3>
+                </header>
+            </div>
+        )
+    }
+}
